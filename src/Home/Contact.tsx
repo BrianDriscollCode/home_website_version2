002e64441c9a969382269fe306b907f9b4ec5dc1
briandriscollcode <bdriscoll407@gmail.com>
2022-09-09 12:14:14 -0700

    adding contact section

--- src/Home/Contact.tsx
+++ src/Home/Contact.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Contact = () => {
 
+    const [ name, setName ] = useState("");
+    const [ email, setEmail ] = useState("");
+    const [ message, setMessage ] = useState("");
+
+    const [inputType, setInputType] = useState("");
 
     return (
 
@@ -16,11 +21,77 @@ const Contact = () => {
                     Always open to hearing about what you need! Say hi, 
                     let's discuss your project, or if you have any questions, let me know!
                 </p>
+
                 <p>
                     Fill in your information in the form below, looking forward to hearing from you!
                 </p> 
 
+                    <form>
+                        <div 
+                            className="contact_input_container" 
+                            id="name_div"
+                            style={{
+
+                                borderColor: inputType === "name" ? 'blue': 'red'
+
+                            }}
+                        >
+                            <label> Name </label>
+                            <input 
+                                value={name} 
+                                onChange={(e) => setName(e.target.value)}
+                                id="nameInput"
+                                onClick={() => setInputType("name")}
+                            />
+                        </div>
+
+                        <div 
+                            className="contact_input_container" 
+                            id="email_div"
+                            style={{
+
+                                borderColor: inputType === "email" ? 'blue': 'red'
+
+                            }}
+                        >
+                            <label> Email </label>
+                            <input 
+                                value={email} 
+                                onChange={(e) => setEmail(e.target.value)} 
+                                id="emailInput"
+                                onClick={() => setInputType("email")}
+                            />
+                        </div>
+
+                        <div 
+                            className="contact_input_container" 
+                            id="message_div"
+                            style={{
+
+                                borderColor: inputType === "message" ? 'blue': 'red'
+
+                            }}
+                        >
+                            <label> Message </label>
+                            <textarea 
+                                value={message} 
+                                onChange={(e) => setMessage(e.target.value)} 
+                                id="messageInput"
+                                onClick={() => setInputType("message")}
+                            >
+                            </textarea>
+                        </div>
+                    </form>
+
+
+                
+                
+
+
             </div>
+            
+            
+
 
         </div>
 
